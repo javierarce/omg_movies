15bba9147a18f4e095cc182403836a045619d07f
Glitch (omg-movies) <none>
2019-02-13 13:17:38 +0000

    💂🍄 Updated with Glitch

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const Bot = require('./bot.js')
+let bot = new Bot()
+
+let Movie = {}
 
 app.use(express.static('public'))
 
@@ -21,98 +24,73 @@ const onError = (error) => {
   console.log(error)
 }
 
+const onGetRhymesForActor = (response) => {
+  if (!response.data || !response.data.length) {
+    console.log(`couldnt find rhymes for ${Movie.actorLastName}`)
+
+    console.log(Movie)
+    let status = bot.generateStatusForActorAndMovie(Movie.newTitle, Movie.actor, Movie.data)
+    bot.publishTweet(status)
+
+    return
+  }
+
+  let lastNameRhyme = bot.getRhymeOrContextFromData(response.data)
+
+  if (lastNameRhyme) {
+    Movie.newActorName = bot.buildNewActorName(Movie.actor, Movie.actorLastName, lastNameRhyme)
+    console.log(Movie)
+    
+    let status = bot.generateStatusForActorAndMovie(Movie.newTitle, Movie.newActorName, Movie.data)
+    bot.publishTweet(status)
+  } else {
+    console.log(Movie)
+    let status = bot.generateStatusForActorAndMovie(Movie.newTitle, Movie.actor, Movie.data)
+    bot.publishTweet(status)   
+  }
+}
+
+const onGetRhymesForMovie = (response) => {
+
+  if (!response.data || !response.data.length) {
+    throw(`couldnt find rhymes for ${Movie.word}`)
+  }
+
+  let titleRhyme = bot.getRhymeOrContextFromData(response.data)
+
+  if (titleRhyme) {
+    Movie.newTitle = bot.getNewTitle(Movie.title, Movie.word, titleRhyme)
+    Movie.actor = bot.getRandomActor(Movie.data)
+    Movie.actorLastName = bot.getActorLastName(Movie.actor)
+
+    bot.getRhymesFor(Movie.actorLastName.toLowerCase()).then(onGetRhymesForActor).catch(onError)  
+  }
+}
+
+const onGetRandomMovie = (response) => {
+
+  if (!response.data) {
+    throw('Couldn\'t find a movie, sorry')    
+  }
+
+  Movie.data = response.data
+  Movie.title = response.data.Title
+
+  if (!Movie.title) {
+    throw('Couldn\'t find a title, sorry')
+  }
+
+  Movie.word = bot.getRandomWord(Movie.title)
+
+  if (!Movie.word) {
+    throw('Couldn\'t find a word in the title, sorry')
+  }
+
+  bot.getRhymesFor(Movie.word.toLowerCase()).then(onGetRhymesForMovie).catch(onError)
+}
+
 app.get('/movie', (request, response) => {
-
-  let movie = undefined
-  let word = undefined
-  let title = undefined
-  let actor = undefined
-  let actorLastName = undefined
-  let newTitle = undefined
-  let newActorName = undefined
-
-  let bot = new Bot()
-
-  const onGetRhymesForActor = (response) => {
-    if (!response.data || !response.data.length) {
-      console.log(`couldnt find rhymes for ${actor}`)
-      let status = bot.generateStatusForActorAndMovie(newTitle, actor, movie)
-      bot.publishTweet(status)
-      console.log(3, status)
-      return
-    }
-
-    let lastNameRhyme = bot.getRhymeOrContextFromData(response.data)
-
-
-    if (lastNameRhyme) {
-      console.log('Lastname rhyme: ', lastNameRhyme)
-
-      if (lastNameRhyme) {
-        newActorName = bot.buildNewActorName(actor, actorLastName, lastNameRhyme)
-
-        console.log('New actor name: ', newActorName)
-
-        let status = bot.generateStatusForActorAndMovie(newTitle, newActorName, movie)
-        bot.publishTweet(status)
-        console.log(1, status)
-      }
-    } else {
-      let status = bot.generateStatusForActorAndMovie(newTitle, actor, movie)
-      bot.publishTweet(status)
-      console.log(2, status)
-    }
-
-    console.log('-----')
-  }
-
-  const onGetRhymesForMovie = (response) => {
-
-    if (!response.data || !response.data.length) {
-      console.log(`couldnt find rhymes for ${word}`);
-      return
-    }
-
-    let titleRhyme = bot.getRhymeOrContextFromData(response.data)
-
-    if (titleRhyme) {
-      newTitle = bot.getNewTitle(title, word, titleRhyme)
-      actorLastName = bot.getActorLastName(actor)
-
-      console.log('Title rhyme: ', titleRhyme)
-      console.log('New title: ', newTitle)
-      console.log('Last name: ', actorLastName)  
-
-      bot.getRhymesFor(actorLastName.toLowerCase()).then(onGetRhymesForActor).catch(onError)  
-    }
-  }
-
-  const onGetRandomMovie = (response) => {
-    movie = response.data
-    title = response.data.Title
-
-    if (!title) {
-      console.log("Couldn't find a title, sorry")
-      return
-    }
-
-    console.log('Movie: ', title)
-
-    word = bot.getRandomWord(title)
-
-    if (!word) {
-      console.log("Couldn't find a word in the title, sorry")
-      return
-    }
-
-    actor = bot.getRandomActor(movie)
-
-    console.log('Actor: ', actor)
-    console.log('Word: ', word)
-
-    bot.getRhymesFor(word.toLowerCase()).then(onGetRhymesForMovie).catch(onError)
-  }
-
+  Movie = {}
   bot.getRandomMovie().then(onGetRandomMovie).catch(onError)
   response.end('ok')
 })
